7a1a123480c95c33069deffc7aa4f6a0e94f5548
Nisharga Kabir <kabirnisharga@gmail.com>
2023-03-25 20:05:13 +0600

    Final

--- data/connection.js
+++ data/connection.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const db_url = process.env.DB_URL;
+const db_url = process.env.DB_URL_LINK;
 
 const connect = () => {
   mongoose
